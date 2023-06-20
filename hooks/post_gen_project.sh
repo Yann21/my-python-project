04d406ca61cd77ae657c698e8b1d62a083ef5873
Yann Hoffmann <yann.hoffmaaann@gmail.com>
2023-06-20 14:40:02 +0200

    Create automatically the git repo

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-pip install jupyter
+pip install -r requirements.txt
+
+git init
+gh repo create --description "{{ cookiecutter.project_short_description }}" \
+  --cookiecutter.visibility -y {{ cookiecutter.project_name }}
