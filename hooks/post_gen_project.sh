90290d5fe66c8b95ab5b39957f280e5ffad13d63
Yann Hoffmann <yann.hoffmaaann@gmail.com>
2023-06-21 13:09:37 +0200

    Add requirements-dev.txt inside folder

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -3,4 +3,4 @@ pip install -r requirements-dev.txt
 
 git init
 gh repo create --description "{{ cookiecutter.project_short_description }}" \
-  --cookiecutter.visibility -y {{ cookiecutter.project_name }}
+  --{{ cookiecutter.visibility }} -y {{ cookiecutter.project_name }}
